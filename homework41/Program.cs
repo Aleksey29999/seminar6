f86e37761d350715ee3babc0655f6d4d86803205
Aleksey <lexx29@yandex.ru>
2023-01-21 23:20:22 +0300

    семинар 6

---
+++ homework41/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Вариант 1 с функцией
+int SummPositiveNumbers (string message)
+{
+   int sum = 0;
+ while (true)
+{
+     Console.WriteLine(message);
+     string Number = (Console.ReadLine());
+      if (int.TryParse(Number, out int N))
+      { 
+      if (N>0) 
+        sum++; 
+        Console.WriteLine("sum=" + sum);
+      }
+    else
+    {
+       Console.WriteLine("конец");
+       Console.WriteLine("положительных чисел введено =" + sum);
+        break;
+    }
+  }
+return  sum;
+}
+int Sum = SummPositiveNumbers ("введите число");
+
+// Вариант 2 без функции
+// while (true)
+// {
+//    Console.WriteLine("Введите число");
+//     string Number = (Console.ReadLine());
+//     if (int.TryParse(Number, out int N))
+//      { 
+//       if (N>0) 
+//       sum++; 
+//       // Console.WriteLine("Number=" + Number);
+//       Console.WriteLine("sum=" + sum);
+//      }
+//    else
+//    {
+//       Console.WriteLine("конец");
+//       Console.WriteLine("sum=" + sum);
+//        break;
+//    }
+//  }
---
+++ homework43/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Вариант 1 без функций
+
+// Console.WriteLine("введите k1");
+// double k1 = Convert.ToInt32(Console.ReadLine ());
+// Console.WriteLine("введите b1");
+// double b1 = Convert.ToInt32(Console.ReadLine ());
+
+// Console.WriteLine("введите k2");
+// double k2 = Convert.ToInt32(Console.ReadLine ());
+// Console.WriteLine("введите b2");
+// double b2 = Convert.ToInt32(Console.ReadLine ());
+
+// double x = (b2-b1) / (k1-k2);
+// double y = k1 * (b2-b1) / (k1-k2) + b1;
+
+// Console.WriteLine( x);
+// Console.WriteLine( y);
+
+// Вариант 2 
+
+(double k, double b) GetTwoNumber (string message)
+{
+    Console.WriteLine("введите b и k");
+    Console.WriteLine();
+    double b = Convert.ToInt32(Console.ReadLine());
+    double k = Convert.ToInt32(Console.ReadLine());
+    return (k,b);
+}
+(double k1, double b1) = GetTwoNumber ("введите первых два числа");
+(double k2, double b2) = GetTwoNumber ("введите вторых два числа");
+ double x = (b2-b1) / (k1-k2);
+ double y = k1 * (b2-b1) / (k1-k2) + b1;
+ Console.WriteLine("х=" + x);
+ Console.WriteLine("y=" + y);
