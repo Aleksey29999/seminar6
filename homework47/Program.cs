d90d2c9cfd5431b16acb1ffa1627b2bec866c268
Aleksey <lexx29@yandex.ru>
2023-01-25 20:56:18 +0300

    Семинар 7

--- homework47/Program.cs
+++ homework47/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Введите число строк массива");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите число строк массива");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число столбцов массива");
 int n = Convert.ToInt32(Console.ReadLine());
--- homework50/Program.cs
+++ homework50/Program.cs
@@ -1,4 +1,11 @@
-﻿
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+// значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
 Console.WriteLine("Введите число строк массива");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число столбцов массива");
@@ -34,7 +41,7 @@ void PrintArray(int[,] array)
 System.Console.WriteLine("введите номер строки массива");
 int Line = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("введите номер столбца массива");
-int Column = Convert.ToInt32(Console.ReadLine());
+int Column = Convert.ToInt32(Console.ReadLine())
 
 if (Line < m && Column < n)
     System.Console.WriteLine("заданный элемент массива =" + array[Line - 1, Column - 1]);
